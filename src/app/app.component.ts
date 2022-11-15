278d5397fb3ec573d0de63b72651ace81eef4dbe
xurxoster23 <jorge.borrego@tcomet.com>
2022-11-15 08:35:41 +0100

    Práctica Angular Directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01_Directiva';
+  title = 'Registro de Usuarios';
+  mensage = "";
+  registro = false;
+  name:string = "";
+  surname:string = "";
+  cargo:string = "";
+  entradas : any[];
+
+
+  constructor(){
+    this.entradas = [
+      {titulo:"Javascript"},
+      {titulo:"Python"},
+      {titulo:"Java"}
+    ]
+  }
+
+  registrarUsuario(): void{
+
+    this.registro = true;
+    this.mensage = "Usuario Registrado con Éxito";
+
+  }
+
 }
+
